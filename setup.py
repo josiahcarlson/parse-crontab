5bc277a425e9b3b8e7ed7a784c6617511b875137
Josiah Carlson <josiah.carlson@gmail.com>
2015-11-06 05:24:36 +0000

    Bump version for PyPI release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
 
 setup(
     name='crontab',
-    version='0.20.2',
+    version='0.20.3',
     description='Parse and use crontab schedules in Python',
     author='Josiah Carlson',
     author_email='josiah.carlson@gmail.com',
